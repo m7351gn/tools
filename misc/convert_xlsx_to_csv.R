77cddb23a2e817b84d7bc628b234353774722ab7
Michele Magni <82044011+m7351gn@users.noreply.github.com>
2023-12-11 21:34:05 +0100

    fixed xl

--- misc/convert_xlsx_to_csv.R
+++ misc/convert_xlsx_to_csv.R
@@ -3,4 +3,4 @@ library(openxlsx)
 file.name <- 'data'
 
 data <- read.xlsx(paste0(file.name, '.xlsx'))
-write.csv(data, paste0(file.name, '.csv', row.names = F))
+write.csv(data, paste0(file.name, '.csv'), row.names = F)
