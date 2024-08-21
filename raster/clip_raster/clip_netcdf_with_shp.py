30dc815b48e9232da0ffb8c3224bda27e2ecb364
Michele Magni <82044011+mikmagni@users.noreply.github.com>
2024-08-21 11:44:21 +0200

    fixed clip example

--- raster/clip_raster/clip_netcdf_with_shp.py
+++ raster/clip_raster/clip_netcdf_with_shp.py
@@ -8,13 +8,13 @@ from get_coordinates_corners import *
 #### paths
 # ~ inputPathGGMInput = '/eejit/depfg/sutan101/data/globgm/downloaded_from_yoda_on_2023-03-13/research-globgm/input/version_1.0'
 # inputPathGGMOutput = '/eejit/depfg/sutan101/data/globgm_groundwater_netcdf_files/v20230512/upscaled_to_5_min/'
-inputPathNC = 'C:/Users/6574882/OneDrive - Universiteit Utrecht/Documents/B-WEX/G-ENE-WS/data/edward/'
-inputPathSHP = 'C:/Users/6574882/OneDrive - Universiteit Utrecht/Documents/B-WEX/G-ENE-WS/input/global_data/vector/USA/CONUS_mask/'
+inputPathNC = ''
+inputPathSHP = ''
 
 #### read files 
 #shp
 # ~ gdf_country = gpd.read_file('Netherlands_shp/Netherlands.shp')
-gdf_country = gpd.read_file(inputPathSHP + 'CONUS_mask.shp')
+gdf_country = gpd.read_file(inputPathSHP + 'turkeydissolve.shp')
 # ~ gdf_nl = gpd.read_file('Netherlands_shp/Netherlands.shp')
 # ~ gdf_conus = gpd.read_file('CONUS_mask/CONUS_mask.shp')
 
@@ -22,7 +22,7 @@ gdf_country = gpd.read_file(inputPathSHP + 'CONUS_mask.shp')
 # ~ print(gdf_conus)
 
 #nc
-with xr.open_dataset(inputPathNC + 'Population_2015_5arcmin.nc', decode_coords = 'all') as file_nc:
+with xr.open_dataset(inputPathNC + 'population_2023.nc', decode_coords = 'all') as file_nc:
 # ~ with xr.open_dataset(inputPathNC + 'groundwater_depth_bottom_30sec_monthly_1958-2015_upscaled_to_5min.nc', decode_coords = 'all') as file_nc:
     
     raster_to_clip = file_nc
@@ -34,7 +34,7 @@ with xr.open_dataset(inputPathNC + 'Population_2015_5arcmin.nc', decode_coords =
 shp_bounds = get_coordinates_corners(gdf_country)
 
 
-nc_varname = 'Population'
+nc_varname = 'population'
 #### slice netcdf
 sliced_nc = raster_to_clip[nc_varname].sel(
             lon=slice(shp_bounds["lon"][0], shp_bounds["lon"][1]),
@@ -53,5 +53,5 @@ sliced_nc_masked = sliced_nc.where(gridded_hydrowaste_masked)
 # ~ print(sliced_nc_masked)
 
 #### save clipped netcdf to disk
-sliced_nc_masked.to_netcdf('population_2015_conus.nc')
+sliced_nc_masked.to_netcdf('population_2023_turkey.nc')
 # ~ sliced_nc_masked.to_netcdf('globgm_bottom_5arcmin_monthly_1950_2015_conus.nc')
